70e0584aac941f1169cdd25e51a5d7e0dfe8afc3
KurochkinVadim <kurochkinv.02@gmail.com>
2025-04-06 20:24:57 +0300

    pr3 dop

--- docker_app/flask_app/app.py
+++ docker_app/flask_app/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
-@app.route('/')
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-def home():
-    return 'Hello, Docker!'
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+
+@app.route('/')
+def index():
+    return "Hello with PostgreSQL!"
