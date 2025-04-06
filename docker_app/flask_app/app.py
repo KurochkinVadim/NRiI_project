e66702255968cd5f0e54c478f38853b776b326b5
KurochkinVadim <kurochkinv.02@gmail.com>
2025-04-06 20:50:21 +0300

    Update ci.yml

--- docker_app/flask_app/app.py
+++ docker_app/flask_app/app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
+
 
 @app.route('/')
 def index():
